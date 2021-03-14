6b10ee29355d4c28013edeb36dc381b0f98add92
Jane <janeridebmx@gmail.com>
2021-03-14 13:42:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(array === null || array === undefined || array.length === 0){
+    return 0;
+  }
+
+  return array.reduce((val, acc) => acc = acc < val ? acc : val, Infinity);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === null || array === undefined || array.length === 0){
+    return 0;
+  }
+
+  return array.reduce((val, acc) => acc = acc > val ? acc : val, -Infinity);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === null || array === undefined || array.length === 0){
+    return 0;
+  }
+
+  return array.reduce((val, acc) => acc += val, 0) / array.length;
 }
